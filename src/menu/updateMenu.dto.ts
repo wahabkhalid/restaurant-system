333736371597e20c7e584cb493e35821458c2958
Wahab Khalid <apple@Apples-MacBook-Pro.local>
2022-10-03 13:40:12 +0500

    branch commit

--- src/menu/updateMenu.dto.ts
+++ src/menu/updateMenu.dto.ts
@@ -2,6 +2,7 @@
 import { IsEnum, IsNotEmpty } from "class-validator";
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMenuDto } from './menu.dto';
+import { ApiProperty } from "@nestjs/swagger";
 enum itemType {
     APPITIZER = 'appitizer',
     PAKISTANI='pakistani',
@@ -11,10 +12,13 @@ enum itemType {
 
 }
 export class UpdateMenuDto extends PartialType(CreateMenuDto) {
+    @ApiProperty()
     @IsNotEmpty()
     item:string;
+    @ApiProperty()
     @IsNotEmpty()
     price:any;
+    @ApiProperty()
     @IsNotEmpty()
     @IsEnum(itemType)
     category:any;
